61df919f708374f707684e011142b961ac9dbdf0
Gaab shiine <gaabshiine@gmail.com>
2024-05-09 15:25:36 +0300

    13rd commit

--- somali_real_estate/settings.py
+++ somali_real_estate/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import secrets
+import string
+
+# Function to generate a random SECRET_KEY
+def generate_secret_key(length=50):
+    alphabet = string.ascii_letters + string.digits + '!@#$%^&*(-_=+)'
+    return ''.join(secrets.choice(alphabet) for _ in range(length))
+
+# Set SECRET_KEY using environment variable or generate a random one
+SECRET_KEY = os.environ.get("SECRET_KEY", generate_secret_key())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +31,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
