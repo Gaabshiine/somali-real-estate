9e380d5e8b3712037b70f1f563a51a0de291bce0
Gaab shiine <gaabshiine@gmail.com>
2024-05-09 18:57:16 +0300

    14rd commit

--- somali_real_estate/settings.py
+++ somali_real_estate/settings.py
@@ -33,10 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"  #when you end of the developement leave the comment
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true" # when you end of the developement leave the comment
 
-
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS").split(" ")]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
