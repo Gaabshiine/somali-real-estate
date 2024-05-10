70f690f97a8a84069735371050476a3e23043025
Gaab shiine <gaabshiine@gmail.com>
2024-05-10 16:48:24 +0300

    16 commits

--- somali_real_estate/settings.py
+++ somali_real_estate/settings.py
@@ -33,8 +33,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get("DEBUG", "False").lower() == "true" # when you end of the developement leave the comment
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true" # when you end of the developement leave the comment
+
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
