2aaa8b894aef7e514bb860f045b52eb0179b924d
Gaab shiine <gaabshiine@gmail.com>
2024-05-15 19:28:56 +0300

    LOGGING

--- somali_real_estate/settings.py
+++ somali_real_estate/settings.py
@@ -56,6 +56,25 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/path/to/django/error.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
